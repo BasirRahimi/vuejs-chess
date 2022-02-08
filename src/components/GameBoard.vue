ec5868384955385eb28da9ef7d9afb50b5722c0f
BasirRahimi <basir@focusimc.co.uk>
2022-02-08 01:38:42 +0000

    game pieces array added

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -16,9 +16,15 @@ type Position = {
   colour: string
 }
 
+type Piece = {
+  name: 'King' | 'Queen' | 'Bishop' | 'Knight' | 'Rook' | 'Pawn',
+  team: 'light' | 'dark',
+  position: string
+}
+
 export default defineComponent({
   setup() {
-    const xAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    const xAxis = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
     const positions:Array<Position> = []
 
     for (let i = 8; i > 0; i--) {
@@ -35,9 +41,111 @@ export default defineComponent({
         }
 
         positions.push({
-          name: `${j}${letter}`,
+          name: `${letter}${i}`,
           colour: colour()
         })
+      })
+    }
+
+    const lights:Array<Piece> = [
+      {
+        name: 'King',
+        team: 'light',
+        position: 'E1'
+      },
+      {
+        name: 'Queen',
+        team: 'light',
+        position: 'D1'
+      },
+      {
+        name: 'Bishop',
+        team: 'light',
+        position: 'C1'
+      },
+      {
+        name: 'Bishop',
+        team: 'light',
+        position: 'F1'
+      },
+      {
+        name: 'Knight',
+        team: 'light',
+        position: 'B1'
+      },
+      {
+        name: 'Knight',
+        team: 'light',
+        position: 'G1'
+      },
+      {
+        name: 'Rook',
+        team: 'light',
+        position: 'A1'
+      },
+      {
+        name: 'Rook',
+        team: 'light',
+        position: 'H1'
+      }
+    ]
+
+    for (let i = 0; i < 7; i++) {
+      lights.push({
+        name: 'Pawn',
+        team: 'light',
+        position: `${xAxis[i]}2`
+      })
+    }
+
+    const darks:Array<Piece> = [
+      {
+        name: 'King',
+        team: 'dark',
+        position: 'E8'
+      },
+      {
+        name: 'Queen',
+        team: 'dark',
+        position: 'D8'
+      },
+      {
+        name: 'Bishop',
+        team: 'dark',
+        position: 'C8'
+      },
+      {
+        name: 'Bishop',
+        team: 'dark',
+        position: 'F8'
+      },
+      {
+        name: 'Knight',
+        team: 'dark',
+        position: 'B8'
+      },
+      {
+        name: 'Knight',
+        team: 'dark',
+        position: 'G8'
+      },
+      {
+        name: 'Rook',
+        team: 'dark',
+        position: 'A8'
+      },
+      {
+        name: 'Rook',
+        team: 'dark',
+        position: 'H8'
+      }
+    ]
+
+    for (let i = 0; i < 7; i++) {
+      darks.push({
+        name: 'Pawn',
+        team: 'dark',
+        position: `${xAxis[i]}7`
       })
     }
 
